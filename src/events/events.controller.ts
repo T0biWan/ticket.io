090900b9558827a9f547e0f6dbdafb891f4799d5
tobi <tobi.klatt@web.de>
2022-03-08 22:04:20 +0100

    Implement crud endpoints for tickets and events

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from "@nestjs/common"
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common"
+import { Ticket } from "src/tickets/tickets.model";
 import { EventsService } from "./events.service";
+
 
 @Controller('events')
 export class EventsController {
-    constructor(private readonly eventsService: EventsService) {}    
+    constructor(private readonly eventsService: EventsService) {}
+
+    @Post()
+    addEvent (@Body('eventTitle') eventTitle: string, @Body('eventDate') eventDate: string, @Body('eventCity') eventCity: string, @Body('tickets') tickets: Ticket) {
+        const newEventId = this.eventsService.addEvent(eventTitle, eventDate, eventCity, tickets)
+        return { id: newEventId }
+    }
+
+    @Get()
+    getEvents () {
+        return this.eventsService.getEvents()
+    }
+
+    @Get(':eventId')
+    getEvent (@Param('eventId') eventId: string) {        
+        return this.eventsService.getEvent(eventId)
+    }
+
+    @Patch(':eventId')
+    updateEvent (@Param('eventId') eventId: string, @Body('eventTitle') eventTitle: string, @Body('eventDate') eventDate: string, @Body('eventCity') eventCity: string, @Body('tickets') tickets: Ticket[]) {
+        this.eventsService.updateEvent(eventId, eventTitle, eventDate, eventCity, tickets)
+    }
+
+    @Delete(':eventId')
+    deleteEvent (@Param('eventId') eventId :string) {
+        this.eventsService.removeEvent(eventId)
+    }
 }
--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,8 +1,35 @@
-import { Controller } from "@nestjs/common"
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common"
 import { TicketsService } from "./tickets.service"
 
 
 @Controller('tickets')
 export class TicketsController {
-    constructor(private readonly ticketsService: TicketsService) {}    
+    constructor(private readonly ticketsService: TicketsService) {} 
+    
+    
+    @Post()
+    addTicket (@Body('eventId') eventId: string, @Body('barcode') barcode: string, @Body('firstName') firstName: string, @Body('lastName') lastName: string) {
+        const newTicketId = this.ticketsService.addTicket(eventId, barcode, firstName, lastName)
+        return { id: newTicketId }
+    }
+
+    @Get()
+    getTickets () {
+        return this.ticketsService.getTickets()
+    }
+
+    @Get(':ticketId')
+    getTicket (@Param('ticketId') ticketId: string) {        
+        return this.ticketsService.getTicket(ticketId)
+    }
+
+    @Patch(':ticketId')
+    updateTicket (@Param('ticketId') ticketId: string, @Body('eventid') eventid: string, @Body('barcode') barcode: string, @Body('firstName') firstName: string, @Body('lastName') lastName: string) {
+        this.ticketsService.updateTicket(ticketId, eventid, barcode, firstName, lastName)
+    }
+
+    @Delete(':ticketId')
+    deleteTicket (@Param('ticketId') ticketId :string) {
+        this.ticketsService.removeTicket(ticketId)
+    }
 }
