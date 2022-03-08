4d1d85e957452aec1a4981dfc9f8bec59bd3d27d
tobi <tobi.klatt@web.de>
2022-03-08 22:03:56 +0100

    remove dev code and generate actual uuids instead of an placeholder

--- src/events/events.model.ts
+++ src/events/events.model.ts
@@ -4,8 +4,7 @@ import {v4 as uuidv4} from 'uuid'
 export class Event {
     id: string; 
  
-    constructor (public eventTitle: string, public eventDate: Date, public eventCity: string, public tickets: [Ticket]) {
-        // this.id = uuidv4()
-        this.id = '13'
+    constructor (public eventTitle: string, public eventDate: string, public eventCity: string, public tickets: Ticket[]) {
+        this.id = uuidv4()
     }
 }
