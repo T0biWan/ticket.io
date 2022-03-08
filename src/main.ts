4ba8179bd348e30d1770c300a3d1d55a9455c317
tobi <tobi.klatt@web.de>
2022-03-08 22:30:21 +0100

    Add auto-generated swagger-api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule)
+
+    const config = new DocumentBuilder()
+        .setTitle('ticket.io')
+        .setDescription('Coding Challenge: Backend & API')
+        .setVersion('1.0')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, document)
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
