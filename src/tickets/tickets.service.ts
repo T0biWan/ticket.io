9a12d9763e3202d82f6031503857f943f0a07232
tobi <tobi.klatt@web.de>
2022-03-08 19:36:46 +0100

    Implement CRUD operations for tickets

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,6 +1,47 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, NotFoundException } from "@nestjs/common"
 import { Ticket } from "./tickets.model"
 
 
 @Injectable()
-export class TicketsService {}
+export class TicketsService {
+    private static tickets: Ticket[] = []
+
+    private findTicket (ticketId: string): [Ticket, number] {
+        const index = TicketsService.tickets.findIndex(ticket => ticket.id === ticketId)
+        const ticket = TicketsService.tickets[index]
+        if (!ticket) throw new NotFoundException(`Could'nt find Ticket with id: ${ticketId}`)
+        return [ticket, index]
+    }
+
+    addTicket (barcode: string, firstName: string, lastName: string) {
+        const newTicket = new Ticket(barcode, firstName, lastName)
+        TicketsService.tickets.push(newTicket)
+                
+        return newTicket.id
+    }
+
+    getTickets () {
+        return [...TicketsService.tickets]
+    }
+
+    getTicket (ticketId: string) {
+        const [ticket, index] = this.findTicket(ticketId)
+        return { ...ticket }
+    }
+
+    updateTicket (ticketId: string, barcode: string, firstName: string, lastName: string) {
+        const [ticket, index] = this.findTicket(ticketId)
+        const updatedTicket = {...ticket}
+        
+        if (barcode) updatedTicket.barcode = barcode
+        if (firstName) updatedTicket.firstName = firstName
+        if (lastName) updatedTicket.lastName = lastName
+
+        TicketsService.tickets[index] = updatedTicket
+    }
+
+    removeTicket (ticketId: string) {
+        const [ticket, index] = this.findTicket(ticketId)
+        TicketsService.tickets.splice(index, 1)
+    }
+}
