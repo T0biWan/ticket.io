22f8756b32ce2c6b91f58f3f84be2db33120074b
tobi <tobi.klatt@web.de>
2022-03-11 17:56:12 +0100

    improve init method with a state clean up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
 import { EventsService } from './events/events.service'
 import { TicketsService } from './tickets/tickets.service'
@@ -7,12 +7,14 @@ import {v4 as uuidv4} from 'uuid'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly ticketsService: TicketsService, private readonly eventsService: EventsService) {}
+    constructor(private readonly appService: AppService, private readonly ticketsService: TicketsService, private readonly eventsService: EventsService) {}
 
+    
     @Post('init')
     init () {
         console.log('[init] create entries in temporary state')
-
+        TicketsService.tickets = []
+        EventsService.events = []
         const newEventId = this.eventsService.addEvent('Royal Award Ceremony', '2022-04-01T20:00:00', 'Yavin 4', [])    
         this.ticketsService.addTicket(newEventId, uuidv4().slice(0, 8), 'Han', 'Solo')
         this.ticketsService.addTicket(newEventId, uuidv4().slice(0, 8), 'Luke', 'Skywalker')
@@ -22,6 +24,4 @@ export class AppController {
         const tickets = this.ticketsService.getTickets()
         this.eventsService.updateEvent(event.id, event.eventTitle, event.eventDate, event.eventCity, tickets)
     }
-
-
 }
